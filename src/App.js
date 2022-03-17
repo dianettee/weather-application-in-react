3d0f37a679c26fd24ddbcbb806b31d2cf904b474
dianettee <dededianeta@gmail.com>
2022-03-17 21:58:20 +0100

    updated project with latest version of dependencies

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <footer>
-        This website is created by Diana Militaru and is{" "}
-        <a
-          href="https://github.com/dianettee/weather-application-in-react"
-          target="_blank"
-        >
-          open-source
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather Application in React</h1>
+        <footer>
+          This website is created by Diana Militaru and is{" "}
+          <a
+            href="https://github.com/dianettee/weather-application-in-react"
+            target="_blank"
+          >
+            open-source
+          </a>
+          <button className="btn btn-danger">HELLO</button>
+        </footer>
+      </div>
     </div>
   );
 }
