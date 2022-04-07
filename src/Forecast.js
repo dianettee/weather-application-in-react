f4048bef9cceb8ff4140966cb4f27dc0c07aaa31
dianettee <dededianeta@gmail.com>
2022-04-07 14:06:04 +0200

    forecast for the first day

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,35 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Forecast.css";
-import WeatherIcon from "./WeatherIcon";
 import axios from "axios";
+import ForecastDay from "./ForecastDay";
 
 export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  //by default the variable will be not loaded (default state) --> false
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
-  console.log(props);
 
-  let lat = props.coordinates.lat;
-  let lon = props.coordinates.lon;
-  let apiKey = "afec290b2afca450b97152096f8e3b9e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="Forecast">
-      <div className="row">
-        <div className="col">
-          <div className="ForecastDay">Mon</div>
-          <div className="WeatherIcon">
-            <WeatherIcon code="01d" size={40} />
-          </div>
-          <div className="ForecastTemperatures">
-            <span className="ForecastTempMin">6°</span>{" "}
-            <span className="ForecastTempMax">15°</span>
+  if (loaded) {
+    // loaded is here a variable
+    return (
+      <div className="Forecast">
+        <div className="row">
+          <div className="col">
+            <ForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "8d124e16fe27fa329c4623b6a16ee83a";
+    let lat = props.coordinates.lat;
+    let lon = props.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+    // a component always has to return something, even nothing - null
+  }
 }
---
+++ src/ForecastDay.js
@@ -0,0 +1,36 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function ForecastDay(props) {
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}`;
+  }
+
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="ForecastDay">{day()}</div>
+      <div className="WeatherIcon">
+        <WeatherIcon code={props.data.weather[0].icon} size={40} />
+      </div>
+      <div className="ForecastTemperatures">
+        <span className="ForecastTempMin">{minTemperature}</span>{" "}
+        <span className="ForecastTempMax">{maxTemperature}°</span>
+      </div>
+    </div>
+  );
+}
