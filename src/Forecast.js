31b3b58fe90e34d480e9f30c06ef836a729e7aab
dianettee <dededianeta@gmail.com>
2022-04-09 14:39:41 +0200

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -33,6 +33,8 @@ export default function Forecast(props) {
                   <ForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
